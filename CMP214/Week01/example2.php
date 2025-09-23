389ab9ff1233fc864d4a9a6f782f6bcf03dfacf2
CrokSoul <potapkin485@gmail.com>
2025-09-23 16:25:57 +0100

    Connected PHP to the Database

--- CMP214/Week01/example2.php
+++ CMP214/Week01/example2.php
@@ -1,5 +1,7 @@
 <?php
-		
+echo "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB' crossorigin='anonymous'>";
+echo "<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js' integrity='sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI' crossorigin='anonymous'></script>";
+
 	$servername = "localhost";
 	$username = "MishaDB";
 	$password = "8uekc55p2vlvPN";
@@ -13,16 +15,16 @@ echo "Connected successfully ";
 
 $sql = "SELECT id, name, email FROM tblUsers";
 $result = $conn->query($sql);
+
 ?>
-<table border = '1'>
+<table class='table table-dark table-hover'>
   
   <tr>
   	<th>ID</th>
   	<th>Name</th>
   	<th>Email</th>
   </tr>
- <?php
-
+<?php
 if ($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
       
@@ -33,8 +35,6 @@ if ($result->num_rows > 0) {
   		<td>".$row["email"]."</td>
       </tr>
       		";
-      
-		echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"];
 	}
 } else {
 	echo "0 results";
@@ -47,10 +47,13 @@ $conn->close();
 
   <head>
     <title>The first web page</title>
+
   </head>
 
   <body>
     <h1>Hello from NCG</h1>
+    
   </body>
 
 </html>
+  
